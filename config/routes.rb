d821c7240d64f4c18805ad8f3602d4cb3d1df4fc
David Deus <davidafonsodeus@outlook.com>
2022-11-17 15:37:50 +0000

    not done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'lists', to: 'lists#index'
+  get 'lists/new', to: 'lists#new'
+  post 'lists', to: 'lists#create'
+  get 'lists/:id', to: 'lists#show', as: 'list'
 end
